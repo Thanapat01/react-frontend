59392bd16d761078fa78dff49653e814c9bbbc40
Thanapat01 <thanapat_hunter@hotmail.com>
2023-07-30 00:01:47 +0700

    change color of navbar button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <div className="flex items-center h-16 p-4 gap-4 border-b-2 border-black bg-slate-200 ">
-      <div className="bg-blue-400 text-white font-bold p-2 rounded-md">
+      <div className="bg-blue-500 text-white font-bold p-2 rounded-md">
         <Link to="/">Home</Link>
       </div>
-      <div className="bg-blue-400 text-white font-bold p-2 rounded-md">
+      <div className="bg-blue-500 text-white font-bold p-2 rounded-md">
         <Link to="/add">Add Todo</Link> 
       </div>
-      <div className="bg-blue-400 text-white font-bold p-2 rounded-md">
+      <div className="bg-blue-500 text-white font-bold p-2 rounded-md">
         <Link to="/view">View Todo</Link>
       </div>
     </div>
